ab5a2d0814932ef10b0fee83c31e6e48378fc4df
seonhyeokjun <tjsrmatn@naver.com>
2023-03-30 13:32:24 +0900

    Initial commit

---
+++ src/main/java/com/toyproject/seonbae/SeonBaeApplication.java
@@ -0,0 +1,13 @@
+package com.toyproject.seonbae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeonBaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeonBaeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toyproject/seonbae/SeonBaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.seonbae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeonBaeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
