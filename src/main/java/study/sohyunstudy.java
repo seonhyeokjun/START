e6711fc276e1c1c721847d81affb8118f48821c6
Baesohyun <qothgusdk@naver.com>
2023-04-05 12:49:11 +0900

    공부중

---
+++ src/main/java/study/sohyunstudy.java
@@ -0,0 +1,13 @@
+package study;
+
+import java.util.Scanner;
+
+public class sohyunstudy {
+    public static void main(String[] args) {
+      int a=1;
+        System.out.println(a+1);
+
+        a=2;
+        System.out.println(a+1);
+    }
+}
