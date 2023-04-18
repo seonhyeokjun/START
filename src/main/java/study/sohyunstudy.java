89d481cfda16a93fed1cd74078fc76defca33aeb
Baesohyun <qothgusdk@naver.com>
2023-04-18 16:46:53 +0900

    sohyun study

--- src/main/java/study/sohyunstudy.java
+++ src/main/java/study/sohyunstudy.java
@@ -3,11 +3,22 @@ package study;
 import java.util.Scanner;
 
 public class sohyunstudy {
+    public static void avg(int x, int y){
+        System.out.println((x+y)/2);
+    }
+    public static void sum(int x, int y){
+        System.out.println(x+y);
+    }
     public static void main(String[] args) {
-      int a=1;
-        System.out.println(a+1);
 
-        a=2;
-        System.out.println(a+1);
+        Scanner left = new Scanner(System.in);
+        Scanner right = new Scanner(System.in);
+
+        int x = left.nextInt();
+        int y = right.nextInt();
+
+        sum(x, y);
+        avg(x, y);
+
     }
 }
